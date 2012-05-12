e581421f354b955687ce87e51ba9ec0f508dc085
Maciej Małecki <maciej.malecki@notimplemented.org>
2012-05-12 12:28:32 +0200

    [refactor test] Refactor `read-dir-files` test

--- test/read-dir-files-test.js
+++ test/read-dir-files-test.js
@@ -3,6 +3,39 @@ var path = require('path'),
     vows = require('vows'),
     readDirFiles = require('../');
 
+var content = {
+  a: new Buffer('Hello world\n'),
+  b: new Buffer('Hello ncp\n'),
+  c: new Buffer(''),
+  d: new Buffer(''),
+  sub: {
+    a: new Buffer('Hello nodejitsu\n'),
+    b: new Buffer('')
+  }
+};
+
+function getContent(content, encoding, recursive) {
+  var encoded = {};
+
+  if (typeof encoding == "boolean") {
+    recursive = encoding;
+    encoding = null;
+  }
+  typeof recursive == "undefined" && (recursive = true);
+  typeof encoding == "string" || (encoding = null);
+
+  Object.keys(content).forEach(function (key) {
+    if (typeof content[key] === 'object' && content[key].constructor !== Buffer) {
+      if (!recursive) {
+        return;
+      }
+      return encoded[key] = getContent(content[key], encoding);
+    }
+    encoded[key] = encoding ? content[key].toString(encoding) : content[key];
+  });
+  return encoded;
+}
+
 vows.describe('read-dir-files/read').addBatch({
   'When using `read-dir-files`': {
     'and reading a directory (`readDirFiles.read("dir", cb)`)': {
@@ -11,16 +44,7 @@ vows.describe('read-dir-files/read').addBatch({
       },
       'it should contain all files': function (data) {
         assert.isObject(data);
-        assert.deepEqual(data, {
-          a: new Buffer('Hello world\n'),
-          b: new Buffer('Hello ncp\n'),
-          c: new Buffer(''),
-          d: new Buffer(''),
-          sub: {
-            a: new Buffer('Hello nodejitsu\n'),
-            b: new Buffer('')
-          }
-        });
+        assert.deepEqual(data, content);
       }
     },
     'and reading a directory (`readDirFiles.read("dir", "utf8", cb)`)': {
@@ -33,16 +57,7 @@ vows.describe('read-dir-files/read').addBatch({
       },
       'it should contain all files': function (data) {
         assert.isObject(data);
-        assert.deepEqual(data, {
-          a: new Buffer('Hello world\n').toString('utf8'),
-          b: new Buffer('Hello ncp\n').toString('utf8'),
-          c: new Buffer('').toString('utf8'),
-          d: new Buffer('').toString('utf8'),
-          sub: {
-            a: new Buffer('Hello nodejitsu\n').toString('utf8'),
-            b: new Buffer('').toString('utf8')
-          }
-        });
+        assert.deepEqual(data, getContent(content, 'utf8'));
       }
     },
     'and non-recursively reading a directory (`readDirFiles.read("dir", false, cb)`)': {
@@ -55,12 +70,7 @@ vows.describe('read-dir-files/read').addBatch({
       },
       'it should contain all files': function (data) {
         assert.isObject(data);
-        assert.deepEqual(data, {
-          a: new Buffer('Hello world\n'),
-          b: new Buffer('Hello ncp\n'),
-          c: new Buffer(''),
-          d: new Buffer('')
-        });
+        assert.deepEqual(data, getContent(content, false));
       }
     }
   }
